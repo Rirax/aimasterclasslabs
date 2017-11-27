1aae0f5cc3f9d291a5bb9cc193f5e9046a01a213
Reynald LECHAPT <rlechapt@e1r4p7.42.fr>
2017-11-27 16:44:39 +0100

    ahah

--- models.py
+++ models.py
@@ -5,9 +5,16 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc0 = nn.Linear(28*28, 27)
+        # self.fc0 = nn.Linear(28*28, 27)
+        # [canal input (1 color), canal output, taille fenetre]
+        self.fc0 = nn.Conv2d(1, 2, 3)
+        self.fc1 = nn.Conv2d(2, 2, 3)
+        self.fc2 = nn.Linear(2*11*11, 27)
 
     def forward(self, x):
-        x = self.fc0(x.view(x.size(0), -1))
-        
+        #pooling
+        x = F.max_pool2d(self.fc0(x), (2,2))
+        x = self.fc1(F.relu(x))
+        x = self.fc2(F.relu(x.view(x.size(0), 2*11*11)))
+
         return F.log_softmax(x)
